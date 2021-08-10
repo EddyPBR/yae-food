028f85c6aaee2fb6e693b0259b3bdc553a5d7021
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-10 16:31:46 -0300

    feat: setup styled-components, global styles and theme

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle, css } from "styled-components";
+
+import Exo2Bold from "@fontsource/exo-2/700.css";
+import RobotoRegular from "@fontsource/roboto/400.css";
+import RobotoBold from "@fontsource/roboto/700.css";
+
+const Fonts = css`
+  ${Exo2Bold}
+  ${RobotoRegular}
+  ${RobotoBold}
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  ${Fonts}
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${props => props.theme.colors.background};
+    font: ${props => props.theme.fonts.text};
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    background: "#FCFCFC",
+  },
+  fonts: {
+    title: "700 3rem 'Exo 2', sans-serif",
+    text: "400 1rem 'Roboto', sans-serif",
+  },
+};

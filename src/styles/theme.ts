152b516d39df1597d02326a40424f96885d29b78
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-11 20:40:44 -0300

    style: updating theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,8 +3,8 @@ export const theme = {
     background: "#FCFCFC",
   },
   fonts: {
-    title: "700 3rem 'Exo 2', sans-serif",
-    text: "400 1rem 'Roboto', sans-serif",
-    bold: "700 1rem 'Roboto', sans-serif",
+    title: "700 4.8rem 'Exo 2', sans-serif",
+    text: "400 1.6rem 'Roboto', sans-serif",
+    bold: "700 1.6rem 'Roboto', sans-serif",
   },
 };
